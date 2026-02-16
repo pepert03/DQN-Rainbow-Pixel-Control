50943d4e455fb03cf53fe5331194785ba8e0e855
pepert03 <202111246@alu.comillas.edu>
2026-02-17 00:22:29 +0100

    Agent working

--- src/dqn.py
+++ src/dqn.py
@@ -4,16 +4,14 @@ import torch.nn.functional as F
 
 
 class DQN(nn.Module):
-    def __init__(self, state_dim, action_dim):
+    def __init__(self, state_dim, action_dim, hidden_dim=256):
         super(DQN, self).__init__()
-        self.fc1 = nn.Linear(state_dim, 128)
-        self.fc2 = nn.Linear(128, 128)
-        self.fc3 = nn.Linear(128, action_dim)
+        self.fc1 = nn.Linear(state_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, action_dim)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        return self.fc3(x)
+        return self.fc2(x)
 
 
 if __name__ == "__main__":
